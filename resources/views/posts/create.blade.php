04aec3a9a06eade29f02678002f87d3dc5759de3
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-12 10:41:24 +0000

    create post to validation.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -22,8 +22,13 @@
                   id="title"
                   name="title"
                   class="form-control"
+                  value="{{old('title')}}"
                   type="text"
                 >
+                <div class="text-danger">
+                  {{$errors->first('title')}}
+                </div>
+                
             </div>
             <div>
                 <label for="body">
@@ -35,8 +40,11 @@
                   name="body"
                   class="form-control"
                   rows="4"
-                  >
+                  >{{old('body')}}
                 </textarea>
+                <div class="text-danger">
+                  {{$errors->first('body')}}
+                </div>
             </div>
             <div class="mt-5">
             <a class="btn btn-secondary" href="{{route('top')}}">
